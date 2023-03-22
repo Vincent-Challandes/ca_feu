5204aeb6923c6252026f4de7d0215598d2b40962
Vincent Challandes <v.challandes@gmail.com>
2023-03-22 15:51:18 +0100

    fini

--- feu02.py
+++ feu02.py
@@ -23,21 +23,20 @@ def rectangle(array):
     return array
 
 def find_shape(board, shape):
+    # la on créer un tableau array_display qui à exacement le format de board mais ou chaque élément est remplacé par "-"
+    # donc il s'agit d'une compréhension de liste 
+    # for row in board donc chaque ligne du board puis on boucle dans chaque ligne et remplace chaque élément par "-" avec "-" for _ in range(len(row))
+    # le _ aurait pu être i ca ne change rien mais par convention si on ne fait rien avec les valeur des élément on met _ c'est tous
+    array_display = [["-" for _ in range(len(row))] for row in board]
     #trouver la taille de la forme x et y
     shape_x = len(shape[0])
     shape_y = len(shape)
     board_x = len(board[0])
     board_y = len(board)
-    print(f"shape_x = {shape_x}")
-    print(f"shape_y = {shape_y}")
-    print(f"board_x = {board_x}")
-    print(f"board_y = {board_y}")
     # de cette manière on délimite jusqu'à quel étage on peut descendre pour trouver le 1 er élément commun sans sortir du board
     for yb in range(board_y - shape_y + 1):
-        print(f"yb = {yb}")
         # de cette manière on délimite jusqu'ou sur la ligne on peut trouver le 1 er élément commun sans sortir du board
         for xb in range(board_x - shape_x + 1):
-            print(f"xb = {xb}")
             found = True
             # va permettre de compté le nombre d'espace au début de la forme pour incrémenté les coordonnées de x à retourner
             start_space = 0
@@ -45,35 +44,31 @@ def find_shape(board, shape):
             first_element = True
             # on va chercher les élément de la forme l'un apres l'autre pour et les comparer avec les éléments du tableau
             for ys in range(shape_y):
-                print(f"ys = {ys}")
                 for xs in range(shape_x):
-                    print(f"xs = {xs}")
                     # lorsque l'on veut donnée les coordonnées de tableau imbriqué on donne en premier les y (étages) et ensuite les x (position dans l'étage)
-                    print(f"shape[ys][xs] = {shape[ys][xs]}")
                     # si l'élément shape est un espace on va a l'élélment suivant
                     if shape[ys][xs] != " " and shape[ys][xs] != board[yb + ys][xb + xs]:
-                        print(f"board[yb + ys][xb + xs] = {board[yb + ys][xb + xs]}")
                         found = False
-                        print("break")
                         break
-                    print(f"board[yb + ys][xb + xs] = {board[yb + ys][xb + xs]}")
-                    print("correspond")
                     # si élément est un espace du début de la forme on incrément notre variable start_space
                     if shape[ys][xs] == " " and first_element:
                         start_space += 1
-                        print("space")
                         first_element = True  
                     # ici on permet de savoir qu'il ya eu élément autre qu'un espace donc nous ne sommes plus au début de la forme on stop l'incrémentation de start_space                      
                     elif shape[ys][xs] != " ":
                         first_element = False
+                        # on ajout au tableau array_display les valeur trouvé pour autant que l'élément de recherche shape[ys][xs] n'est pas un espace
+                        array_display[yb + ys][xb + xs] = shape[ys][xs]
                 # permet de remonté au boucle tu tableau board pour parcourir l'élément suivant du board
                 if not found:
                     break
             if found:
                 if start_space > 0:
-                    return xb + start_space, yb
+                    return xb + start_space, yb, array_display
                 else :
-                    return xb, yb
+                    return xb, yb, array_display
+            # ici on écrase a nouveau les valeur avec "-" car nous avons pas trouvé la forme entière donc on passe a l'élément suivant du tableau.
+            array_display[yb][xb] = "-"
     return None
 
 def print_position(position):
@@ -81,8 +76,14 @@ def print_position(position):
     if position:
         print("Trouvé !")
         print(f"Coordonnées : {position[0]},{position[1]}")
+        print_2d_array(position[2])
     else:
         print("Introuvable")
+
+def print_2d_array(array):
+    for row in array:
+        # on concatenate tous les élément d'une ligne en une chaine de caratère et la print
+        print("".join(row))
                    
 ## Error handling
 error_handling(sys.argv)
@@ -90,15 +91,12 @@ error_handling(sys.argv)
 ## Parsing
 board = read_file(sys.argv[1])
 shape = read_file(sys.argv[2])
-print(board)
-print(shape)
-board = rectangle(board)
-shape = rectangle(shape)
-print(board)
-print(shape)
 
 ## Resolution
+board = rectangle(board)
+shape = rectangle(shape)
 position = find_shape(board, shape)
 
 ## Display
 print_position(position)
+    
